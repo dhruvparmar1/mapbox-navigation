650fa10a03bd93c6510cadaea38c61fa0ad76de5
dhruv <dhruv.parmar1432@gmail.com>
2023-04-11 00:30:08 +0530

    ci: initial commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,56 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 
-import { StyleSheet, View } from 'react-native';
-import { MapboxNavigationView } from 'react-native-mapbox-navigation';
+import {
+  StyleSheet,
+  View,
+  PermissionsAndroid,
+  Platform,
+  Alert,
+  Permission,
+} from 'react-native';
+import MapboxNavigation from 'react-native-mapbox-navigation';
 
 export default function App() {
+  useEffect(() => {
+    Platform.OS === 'android' && requestLocationPermission();
+  }, []);
+
+  const requestLocationPermission = async () => {
+    try {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION as Permission
+      );
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <MapboxNavigationView color="#32a852" style={styles.box} />
+      <MapboxNavigation
+        origin={[-105.140629, 39.760194]}
+        destination={[-105.156544, 39.761801]}
+        style={styles.box}
+        shouldSimulateRoute
+        showsEndOfRouteFeedback
+        hideStatusView
+        onLocationChange={(event) => {
+          console.log('onLocationChange', event.nativeEvent);
+        }}
+        onRouteProgressChange={(event) => {
+          console.log('onRouteProgressChange', event.nativeEvent);
+        }}
+        onArrive={() => {
+          Alert.alert('You have reached your destination');
+        }}
+        onCancelNavigation={() => {
+          Alert.alert('Cancelled navigation event');
+        }}
+        onError={(event) => {
+          const message = event?.nativeEvent?.message;
+          if (message) Alert.alert(message);
+        }}
+      />
     </View>
   );
 }
@@ -18,8 +62,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box: {
-    width: 60,
-    height: 60,
+    width: '100%',
+    height: '100%',
     marginVertical: 20,
   },
 });
